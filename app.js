6479615a6573bfebb75883d0fa5fd018bb3ced14
Jordy Groote <jordygroote@live.nl>
2017-07-06 10:04:17 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var server = require('http').Server(app);
+
+var io = require('socket.io')(server);
+server.listen(3000);
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+
+var time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+
+setInterval(function() {
+
+    time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    io.sockets.emit('timer', {
+        time : time
+    });
+
+}, 2500);
+
+io.sockets.on('connection', function (socket) {
+
+    // socket.on('reset', function (data) {
+    //     countdown = 1000;
+    //     io.sockets.emit('timer', { countdown: countdown });
+    // });
+
+});
